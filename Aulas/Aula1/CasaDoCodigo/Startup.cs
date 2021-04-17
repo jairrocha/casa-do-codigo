93741d6b8388a37cae0217e66d871f4d70c3545c
Jair <jairmsn@hotmail.com>
2021-04-17 15:54:01 -0300

    Adicionando modelos, BD e serviços.

--- Aulas/Aula1/CasaDoCodigo/Startup.cs
+++ Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,12 +22,30 @@ namespace CasaDoCodigo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            /*
+             * Esse método serve para adicionar o serviços
+             */
+
             services.AddMvc();
+
+            //Obtendo connection string definida no 'appsettings.json'
+            string connectionString = 
+                            Configuration.GetConnectionString("Default");
+
+            //Adicionando o serviço de context a aplicação
+            services.AddDbContext<ApplicationContext>(options => 
+                     options.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            IServiceProvider serviceProvider)
         {
+
+            /*
+             * Esse método define os serviço que vamos utilizar
+             */
+
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
@@ -45,6 +64,26 @@ namespace CasaDoCodigo
                     name: "default",
                     template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
+
+            /*
+            * O método abaixo garante que o banco esteja criado, ou seja 
+            * se o banco não existir o método criar o BD com base nos 
+            * arquivos de Migrations.
+            */
+            
+            serviceProvider
+                .GetService<ApplicationContext>()
+                .Database
+                .Migrate();
+
+            /* O método abaixo tbm cria o BD, porém não é remomendávável
+             * devído não utilizar o arquivo de migrations, ou seja, e se
+             * baseia no modelo. O problema disso é uma vez que utiliza ele,
+             * vc não pode aplicar nenhuma migração ao seu projeto. Utilize
+             * o '.Migrate()' pois ele sim utiliza os arquivo de migração.
+             */
+
+            //.EnsureCreated(); 
         }
     }
 }
